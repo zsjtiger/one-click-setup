e513b1d6bd10fe85f796f947f949bf4edfd1d3c6
Kui Xu <kuixu@10.1.12.69>
2022-07-31 17:14:59 +0800

    fixed bug

--- conda/conda.sh
+++ conda/conda.sh
@@ -1,43 +1,18 @@
-environment_package_name='XX'
-
 install_conda()
 {
-    echo "Installing a fresh version of Miniforge3."
-    # Keep variable names in sync with 
-    # https://github.com/conda-forge/docker-images/blob/main/scripts/run_commands
-    miniforge_arch="$(uname -m)"
-    miniforge_version="4.10.3-10"
-    condapkg="https://github.com/conda-forge/miniforge/releases/download/${miniforge_version}/Mambaforge-${miniforge_version}-MacOSX-${miniforge_arch}.sh"
-    if [ "$(uname -m)" = "x86_64" ]; then
-    conda_chksum="7c44259a0982cd3ef212649678af5f0dd4e0bb7306e8fffc93601dd1d739ec0b"
-    elif [ "$(uname -m)" = "arm64" ]; then
-    conda_chksum="72bc86612ab9435915b616c2edb076737cbabe2c33fd684d58c2f9ae72e1957c"
-    else
-    exit 1
-    fi
-    curl -s -L "$condapkg" > miniconda.sh
-    openssl sha256 miniconda.sh | grep $conda_chksum
-
-    bash miniconda.sh -b -p ~/conda
-
-    echo "Configuring conda."
-    # shellcheck disable=SC1090
-    source ~/conda/bin/activate root
-
-    export CONSTRUCT_ROOT="${PWD}"
-    mkdir -p build
-
-    bash build.sh
+    brew install miniforge
+    conda init zsh
 }
 
 config_environments()
 {
-    echo "done"
+   conda create -n chinasfa python=2.7
+   conda install -c http://127.0.0.1:1313/ -n chinasfa chinasfa
 }
 
 
 # 1. install conda
 install_conda
-
 # config environments
+config_environments
 
